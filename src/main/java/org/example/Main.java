735f418c17939d40ecf84812290fcc8835914a42
ahmetdemirelli <ahmetdemirelli@gmail.com>
2024-09-24 08:49:19 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Exit night, enter ligth");
+        System.out.println("Take this course ");
+        System.out.println("We are of to the never never land");
+        for (int i = 0; i < 10; i++) {
+            System.out.println("That is enough today!!!");
+        }
+    }
+}
